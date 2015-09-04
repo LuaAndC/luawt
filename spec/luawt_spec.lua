7437f838cf4886565d5038cf8b6eed6164514be3
Boris Nagaev <bnagaev@gmail.com>
2015-09-04 23:46:00 +0300

    Add spec
    To install, run
    $ luarocks make --local
    
    To check, run
    $ busted

---
+++ spec/luawt_spec.lua
@@ -0,0 +1,11 @@
+-- luawt, Lua bindings for Wt
+-- Copyright (c) 2015 Pavel Dolgov and Boris Nagaev
+-- See the LICENSE file for terms of use.
+
+describe("luawt", function()
+
+    it("requires main module", function()
+        local luawt = require 'luawt'
+    end)
+
+end)
