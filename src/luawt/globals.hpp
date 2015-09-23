a884063322ffa1b446b03ceb59bd6d6d16ffe6fe
Pavel Dolgov <pdolgov99@gmail.com>
2015-09-23 20:48:19 +0300

    template<lua_CFunction F>; struct wrap
    This class converts lua_CFunction which can throw C++
    exceptions to lua_CFunction which can't throw exceptions.
    It catches exceptions and calls lua_error(L).

---
+++ src/luawt/globals.hpp
@@ -0,0 +1,19 @@
+/* luawt, Lua bindings for Wt
+ * Copyright (c) 2015 Pavel Dolgov and Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+template<lua_CFunction F>
+struct wrap {
+    static int func(lua_State* L) {
+        try {
+            return F(L);
+        } catch (std::exception& e) {
+            lua_pushstring(L, e.what());
+        } catch (...) {
+            lua_pushliteral(L, "Unknown exception");
+        }
+        return lua_error(L);
+    }
+};
