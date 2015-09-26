0ce968f108a8ca39dc2fc63d5c16b452fe8a1fe1
Pavel Dolgov <pdolgov99@gmail.com>
2015-09-26 23:29:56 +0300

    Macros my_setfuncs(L, funcs)
    If LUA_VERSION_NUM == 501 use luaL_register.
    Else use luaL_setfuncs.

--- src/luawt/globals.hpp
+++ src/luawt/globals.hpp
@@ -4,7 +4,18 @@
  * See the LICENSE file for terms of use.
  */
 
+#ifndef GLOBALS_HPP_
+#define GLOBALS_HPP_
+
+#include <exception>
+
 #include <lua.hpp>
+
+#if LUA_VERSION_NUM == 501
+#define my_setfuncs(L, funcs) luaL_register(L, NULL, funcs)
+#else
+#define my_setfuncs(L, funcs) luaL_setfuncs(L, funcs, 0)
+#endif
 
 template<lua_CFunction F>
 struct wrap {
@@ -19,3 +30,5 @@ struct wrap {
         return lua_error(L);
     }
 };
+
+#endif
