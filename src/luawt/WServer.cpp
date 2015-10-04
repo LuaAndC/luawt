c48ae5711998f33ec2d216ebad0aa86efc3833ca
Pavel Dolgov <pdolgov99@gmail.com>
2015-10-04 23:49:37 +0300

    WServer.WRun passes port in argv
    Example:
    luawt.WServer.WRun({code = 'qwerty', port = 1111})

--- src/luawt/WServer.cpp
+++ src/luawt/WServer.cpp
@@ -39,12 +39,25 @@ extern "C" {
 */
 int luawt_WServer_WRun(lua_State* L) {
     luaL_checktype(L, 1, LUA_TTABLE);
+    // get code
     lua_getfield(L, 1, "code");
     size_t code_len;
     const char* code = luaL_checklstring(L, 2, &code_len);
-    int argc = 1;
-    char* argv[] = {"", NULL};
-    WRun(argc, argv, LuaAppCreator(std::string(code, code_len)));
+    lua_pop(L, 1);
+    // get port
+    lua_getfield(L, 1, "port");
+    const char* port = luaL_checkstring(L, -1);
+    // make argc, argv
+    typedef std::vector<const char*> Options;
+    Options opt;
+    opt.push_back("luawt");
+    opt.push_back("--http-address=127.0.0.1");
+    opt.push_back("--http-port");
+    opt.push_back(port);
+    opt.push_back("--docroot=/usr/share/Wt");
+    opt.push_back(0);
+    WRun(opt.size() - 1, const_cast<char**>(&opt[0]),
+         LuaAppCreator(std::string(code, code_len)));
     return 0;
 }
 
