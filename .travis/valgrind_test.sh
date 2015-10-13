897fcf979fb25b665c578052f6cd7e27a840aa93
Boris Nagaev <bnagaev@gmail.com>
2015-10-14 01:05:09 +0300

    run under valgrind in travis

---
+++ .travis/valgrind_test.sh
@@ -0,0 +1,9 @@
+echo 'os.exit = function() end' > exitless-busted
+echo 'require "busted.runner"({ batch = true })' >> exitless-busted
+valgrind \
+    --error-exitcode=1 \
+    --leak-check=full \
+    --gen-suppressions=all \
+    lua \
+    exitless-busted --sort
+rm exitless-busted
