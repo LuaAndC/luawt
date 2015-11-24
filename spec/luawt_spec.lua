7fec81bc6d0273a8ffe3d45f85000a0452910ebe
Pavel Dolgov <pdolgov99@gmail.com>
2015-11-24 20:15:54 +0300

    Simplify application test
    By using baseConfig(), socketRequest() and createServer().

--- spec/luawt_spec.lua
+++ spec/luawt_spec.lua
@@ -68,33 +68,18 @@ describe("luawt", function()
 
     it("creates simple application", function()
         local luawt = require 'luawt'
-        local wt_config = os.tmpname()
-        local file = io.open(wt_config, 'w')
-        local config = [=[
-            <server>
-                <application-settings location="*">
-                    <progressive-bootstrap>
-                        true
-                    </progressive-bootstrap>
-                </application-settings>
-            </server> ]=]
-        file:write(config)
-        file:close()
-        local server = luawt.WServer({
-            code = [[
-                local app, env = ...
-                local luawt = require 'luawt'
-                local text = "IP: " .. env:clientAddress()
-                local button = luawt.WPushButton(app:root())
-                button:setText(text)
-            ]],
-            port = 56789,
-            wt_config = wt_config,
-        })
+        local code = [[
+            local app, env = ...
+            local luawt = require 'luawt'
+            local text = "IP: " .. env:clientAddress()
+            local button = luawt.WPushButton(app:root())
+            button:setText(text)
+        ]]
+        local port = 56789
+        local wt_config = baseConfig()
+        local server = createServer(code, port, wt_config)
         server:start()
-        os.execute("sleep 2")
-        local socket = require 'socket.http'
-        local data = socket.request('http://127.0.0.1:56789/')
+        local data = socketRequest(port)
         assert.truthy(data:match('IP:'))
         server:stop()
         os.remove(wt_config)
