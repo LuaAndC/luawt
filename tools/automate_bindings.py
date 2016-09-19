494df29e4b5c0f419ffda8e2e2a0213f1ded92e6
Pavel Dolgov <pdolgov99@gmail.com>
2016-09-19 17:00:23 +0300

    Some simple functions for generating function names
    Just tasting Python.

--- tools/automate_bindings.py
+++ tools/automate_bindings.py
@@ -7,7 +7,32 @@ of given class (types of arguments and return values).
 """
 
 import argparse
+import os
 
+MAIN_FUNC_DEF = r'''
+%s;
+'''
+
+MAIN_FUNC_IMPL = r'''
+%s {
+}
+'''
+
+def get_func_name(module_name):
+    return "void luawt_%s(lua_State* L)" % module_name
+
+def generate_skeleton(module_name):
+    with open('src/luawt/%s.cpp' % module_name, 'wt') as f:
+        f.write(MAIN_FUNC_IMPL % get_func_name(module_name))
+    with open('src/luawt/globals2.hpp', 'w') as f:
+        f.write(MAIN_FUNC_DEF % get_func_name(module_name))
+
+def get_module_name(filename):
+    return os.path.basename(filename)
+
+def bind(to_bind):
+    with open(to_bind, 'r') as f:
+        code = f.read()
 
 def main():
     parser = argparse.ArgumentParser(
@@ -20,6 +45,10 @@ def main():
         help='Header file (Wt) with class to bind',
         required=True,
     )
+    args = parser.parse_args()
+    module_name = get_module_name(args.bind)
+    generate_skeleton(module_name)
+    bind(args.bind)
 
 if __name__ == '__main__':
     main()
