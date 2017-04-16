8d7b28032852b57abdf6e3154d5de6806394d017
Pavel Dolgov <pdolgov99@gmail.com>
2017-04-16 23:58:09 +0300

    Add Global.hpp
    Header for global typedefs and (if any) declarations.

---
+++ src/luawt/Global.hpp
@@ -0,0 +1,18 @@
+/* luawt, Lua bindings for Wt
+ * Copyright (c) 2015-2017 Pavel Dolgov and Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef GLOBAL_HPP_
+#define GLOBAL_HPP_
+
+#include <set>
+#include <vector>
+
+// Useful typedefs.
+typedef long long int lint;
+
+typedef std::set<const char*> StrsSet;
+
+#endif
