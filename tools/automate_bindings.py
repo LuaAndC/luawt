a945c6f0d4427ed4889573acabac4a94ffc0b59f
Pavel Dolgov <pdolgov99@gmail.com>
2017-04-20 22:43:58 +0300

    generateSpecialEnumsSet(): add '\n'

--- tools/automate_bindings.py
+++ tools/automate_bindings.py
@@ -937,7 +937,7 @@ def generateSpecialEnumsSet(special_enums):
     name = 'luawt_SpecialEnums_arr'
     end = '%s + %s' % (name, len(special_enums))
     code += ENUM_STRING_ARRAY_TEMPLATE % (name, body)
-    code += 'StrsSet luawt_SpecialEnums(%s, %s);' % (name, end);
+    code += 'StrsSet luawt_SpecialEnums(%s, %s);\n' % (name, end);
     return code
 
 def generateEnumArrays():
